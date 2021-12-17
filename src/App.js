af574446a9bc01c35cacac36e8899334683588be
Renan Wilson <renan.alves.ra16@gmail.com>
2021-12-17 13:59:17 -0300

    parte 2 erro api ainda

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './index.css';
+import { FiSearch} from 'react-icons/fi';
+import api from './services/api';
 
 function App() {
+  const [input, setInput] = useState('')
+
+  async function handleSearch(){
+    if(input === ''){
+      alert("Preencha com algum cep")
+      return;
+    }
+
+    try{
+
+      const response = await api.get('${input}/JSON');
+
+      console.log(response)
+    }catch{
+
+      alert("erro")
+    }
+  }
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <>
+    {/* HEADER */}
+    <header class="header">
+     <a href='c'> Busca Cep </a>
+    </header>
+    
+    {/* BODY */}
+    <div className="Container">
+
+
+    {/* CLASSE COM TEXT BOX*/}
+    <div className='containerInput'>
+    
+       <input type="text" placeholder='Digite seu cep'
+        value={input} onChange={(e) => setInput(e.target.value)}/>
+
+       <button className='buttonSearch' onClick={handleSearch}>
+         <FiSearch size={25}/>
+       </button>
+     
     </div>
+    
+    {/* CLASSE PRINCIPAL*/}
+    <main className='main'>
+    <h2> cep: </h2>
+      <span>Rua teste algust</span>
+      <span>complemento: algum complemento</span>
+      <span> Vila Rosa</span>
+      <span> São Paulo</span>
+    </main>
+    </div>
+  
+    </>
   );
 }
 
