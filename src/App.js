6f7cb634c719036120d974bfbedc9ccda64c70b1
renanwilson <renan.alves.ra16@gmail.com>
2022-11-01 10:41:04 -0300

    fix: remove api error

--- src/App.js
+++ src/App.js
@@ -1,62 +1,59 @@
-import { useState } from 'react';
-import './index.css';
-import { FiSearch} from 'react-icons/fi';
-import api from './services/api';
+import { useState } from "react";
+import "./index.css";
+import { FiSearch } from "react-icons/fi";
+import api from "./services/api";
 
 function App() {
-  const [input, setInput] = useState('')
+  const [input, setInput] = useState("");
+  const [data, setData] = useState("");
+  const [apiCalled, setApiCalled] = useState(false);
 
-  async function handleSearch(){
-    if(input === ''){
-      alert("Preencha com algum cep")
+  async function handleSearch() {
+    if (input === "") {
+      alert("Preencha com algum cep");
       return;
     }
 
-    try{
-
-      const response = await api.get('${input}/JSON');
-
-      console.log(response)
-    }catch{
-
-      alert("erro")
+    try {
+      await api.get(`${input}/json`).then((response) => {
+        setData(response.data);
+        setApiCalled(true);
+      });
+      console.log(data);
+    } catch {
+      alert();
     }
   }
-    
+
   return (
-    
     <>
-    {/* HEADER */}
-    <header class="header">
-     <a href='c'> Busca Cep </a>
-    </header>
-    
-    {/* BODY */}
-    <div className="Container">
+      {/* HEADER */}
+      <header className="header">
+        <a href="c"> Busca Cep </a>
+      </header>
 
+      <div className="Container">
+        <div className="containerInput">
+          <input
+            type="text"
+            placeholder="Digite seu cep"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
 
-    {/* CLASSE COM TEXT BOX*/}
-    <div className='containerInput'>
-    
-       <input type="text" placeholder='Digite seu cep'
-        value={input} onChange={(e) => setInput(e.target.value)}/>
-
-       <button className='buttonSearch' onClick={handleSearch}>
-         <FiSearch size={25}/>
-       </button>
-     
-    </div>
-    
-    {/* CLASSE PRINCIPAL*/}
-    <main className='main'>
-    <h2> cep: </h2>
-      <span>Rua teste algust</span>
-      <span>complemento: algum complemento</span>
-      <span> Vila Rosa</span>
-      <span> São Paulo</span>
-    </main>
-    </div>
-  
+          <button className="buttonSearch" onClick={handleSearch}>
+            <FiSearch size={25} />
+          </button>
+        </div>
+        {apiCalled && (
+          <main className="main">
+            <h2>cep: {data.cep}</h2>
+            <span>Rua: {data.logradouro}</span>
+            <span>Bairro: {data.bairro}</span>
+            <span> Cidade: {data.localidade}</span>
+          </main>
+        )}
+      </div>
     </>
   );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: "https://viacep.com.br/"
-
+  baseURL: "https://viacep.com.br/ws/",
 });
 export default api;
